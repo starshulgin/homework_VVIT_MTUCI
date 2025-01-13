4154dc276039ecd81a0df4cadfedbd2936b74e5e
starshulgin <starshulgin@gmail.com>
2025-01-13 22:12:52 +0300

    Update my_module.py

--- LAB4/my_module.py
+++ LAB4/my_module.py
@@ -3,3 +3,10 @@ from datetime import datetime
 current_datetime = datetime.now()
 
 print("Текущая дата и время:", current_datetime)
+
+def numberSqrt(num):
+    num *= num
+    return num
+
+def enterString(name):
+    return name
