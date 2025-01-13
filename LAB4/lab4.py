3580d358c204adf5b142e60f959a5a8e455c94f9
starshulgin <starshulgin@gmail.com>
2025-01-13 22:15:16 +0300

    Create lab4.py

---
+++ LAB4/lab4.py
@@ -0,0 +1,15 @@
+from my_module import *
+import datetime
+
+num=int(input("Введите свой номер: "))
+
+name = str(input("Введите свое имя: "))
+
+
+dt_now = datetime.datetime.now()
+
+print(dt_now)
+
+print(numberSqrt(num))
+
+print(enterString(name))
